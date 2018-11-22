ff63829a8cba53175b30d04e709412d0c509b21f
jacob wick <jacob.wick@gmail.com>
2018-11-22 15:57:38 -0600

    Removed wp-content/uploads

--- wp-content/themes/accelerate-theme/header.php
+++ wp-content/themes/accelerate-theme/header.php
@@ -16,6 +16,8 @@
 	<meta name="description" content="A WordPress portfolio project site, built with Skillcrush, featuring custom post types, custom queries, and custom fields.">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/brands.css" integrity="sha384-QT2Z8ljl3UupqMtQNmPyhSPO/d5qbrzWmFxJqmY7tqoTuT2YrQLEqzvVOP2cT5XW" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/fontawesome.css" integrity="sha384-u5J7JghGz0qUrmEsWzBQkfvc8nK3fUT7DCaQzNQ+q4oEXhGSx+P2OqjWsfIRB8QT" crossorigin="anonymous">
 
 	<?php wp_head(); ?>
 </head>
